5cb77d668689efe79592cd14594090dac49a0c4e
Giichi Sasaki <sasaki.giichi@gmail.com>
2012-01-01 06:36:56 +0900

    file追加

---
+++ AkeomeAppTests/AkeomeAppTests.h
@@ -0,0 +1,13 @@
+//
+//  AkeomeAppTests.h
+//  AkeomeAppTests
+//
+//  Created by 義一郎 佐々木 on 12/01/01.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AkeomeAppTests : SenTestCase
+
+@end
---
+++ AkeomeAppTests/AkeomeAppTests.m
@@ -0,0 +1,32 @@
+//
+//  AkeomeAppTests.m
+//  AkeomeAppTests
+//
+//  Created by 義一郎 佐々木 on 12/01/01.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "AkeomeAppTests.h"
+
+@implementation AkeomeAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AkeomeAppTests");
+}
+
+@end
